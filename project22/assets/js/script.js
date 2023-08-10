a1681107c30681f9dc334999b266b2780c995f68
Gleriston <xpanium@gmail.com>
2023-08-10 20:53:44 -0300

    script of A Website Comming Soon Page

---
+++ project22/assets/js/script.js
@@ -0,0 +1,24 @@
+const countDownDate = new Date('Oct 10, 2023 00:00:00').getTime();
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerText = days;
+  document.getElementById('hours').innerText = hours;
+  document.getElementById('minutes').innerText = minutes;
+  document.getElementById('seconds').innerText = seconds;
+
+  if(distance < 0){
+    clearInterval(x);
+    document.getElementById('days').innerText = '00';
+    document.getElementById('hours').innerText = '00';
+    document.getElementById('minutes').innerText = '00';
+    document.getElementById('seconds').innerText = '00';
+  }
+
+}, 1000);
